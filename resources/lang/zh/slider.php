1e7ef08d002705dd6124928ead64caa92390f6fa
ekaardilahfebriyanti <ardilaheka@gmail.com>
2025-07-08 11:37:49 +0700

    init

---
+++ resources/lang/zh/slider.php
@@ -0,0 +1,14 @@
+<?php
+return [
+    'slide_1_title' => '一款应用管理X、IG、WhatsApp等多个平台',
+    'slide_1_desc' => '轻松安排帖子、发送WhatsApp消息并提升互动率。',
+    'slide_1_cta' => '试用演示',
+    'slide_2_cta' => '立即订阅',
+
+    'slide_2_title' => '难以管理所有社交媒体和WhatsApp账号？',
+    'slide_2_desc' => 'Digimar帮助您通过一个仪表板管理所有社交媒体和WhatsApp消息。无需频繁切换应用。一体化解决方案。您还可以获得AI的协助。',
+
+    'slide_3_title' => 'ig.digimar.id – 通过Instagram打造强大品牌',
+    'slide_3_desc' => '管理Reels、Stories及视觉内容，通过Digimar增强互动并提升销售。',
+
+];
